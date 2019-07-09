3a88fb817626ccdfa51cc5186025d09d018307aa
Thulani <thulanig@bettech.com>
2019-07-09 16:44:09 +0200

    alignment changes

--- public/views/player.js
+++ public/views/player.js
@@ -1,49 +1,49 @@
-    // declare variables
-    var playerContainer  = document.getElementById("player-info");
-    var btn  = document.getElementById("btn");
-    var sbmt  = document.getElementById("sbmt")
-    var url = 'http://localhost:3002/team/';
+// declare variables
+var playerContainer  = document.getElementById("player-info");
+var btn  = document.getElementById("btn");
+var sbmt  = document.getElementById("sbmt")
+var url = 'http://localhost:3002/team/';
 
-    // get player info
-    btn.addEventListener("click", function() {
-      var srch_name = document.getElementById("search").value;
-      var request = new XMLHttpRequest();
-      request.open('GET', url+srch_name, true);
-      request.onload = function() {
-        var ourData = JSON.parse(request.responseText);
-        //alert(ourData);
-        renderHTML(ourData);
-      };
-      request.setRequestHeader("Content-type", "application/json");
-      //request.withCredentials = false;
-      request.send();
-    });
-
-      var addForm = document.getElementById("addForm");
-
-    //add player info
-    addForm.addEventListener("submit", function(e) {
-      e.preventDefault();
-    var plyr_name = document.getElementById("name").value;
-    var plyr_surname = document.getElementById("surname").value;
-    var plyr_position = document.getElementById("position").value;
-    var plyr_age = document.getElementById("age").value;
-
-    var player = {
-      name: plyr_name,
-      surname: plyr_surname,
-      position: plyr_position,
-      age: plyr_age
-    }
-    
-    console.log(player)
-
+// get player info
+btn.addEventListener("click", function() {
+  var srch_name = document.getElementById("search").value;
   var request = new XMLHttpRequest();
-
-  request.open('POST', url);
+  request.open('GET', url+srch_name, true);
   request.onload = function() {
-    console.log(request.responseText);
-  }
+    var ourData = JSON.parse(request.responseText);
+    //alert(ourData);
+    renderHTML(ourData);
+  };
+  request.setRequestHeader("Content-type", "application/json");
+  //request.withCredentials = false;
+  request.send();
+});
+
+var addForm = document.getElementById("addForm");
+
+//add player info
+addForm.addEventListener("submit", function(e) {
+  e.preventDefault();
+var plyr_name = document.getElementById("name").value;
+var plyr_surname = document.getElementById("surname").value;
+var plyr_position = document.getElementById("position").value;
+var plyr_age = document.getElementById("age").value;
+
+var player = {
+  name: plyr_name,
+  surname: plyr_surname,
+  position: plyr_position,
+  age: plyr_age
+}
+
+console.log(player)
+
+var request = new XMLHttpRequest();
+
+request.open('POST', url);
+request.onload = function() {
+console.log(request.responseText);
+}
 
 request.setRequestHeader("Content-Type", "application/json");
     var content  = JSON.stringify({plyr: player});
